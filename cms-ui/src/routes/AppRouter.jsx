c5bc32ded4c8f202c6915965c2b5654af65f46b0
Koshal Shrestha <koshalshrestha4@gmail.com>
2024-08-04 16:50:30 +0545

    initiating FrontEnd

---
+++ cms-ui/src/routes/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { Layout } from "@/components"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import * as Pages from "@/pages"
+
+export const AppRouter = () => {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element = {<Layout/>} >
+                <Route index element = {<Pages.Dashboard.Home />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
+}
